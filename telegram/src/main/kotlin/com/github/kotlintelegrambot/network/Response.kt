0003b1d2a5a6eb7b4b3051fc06677bda3fc046a8
FlyJingFish <tianxiangyu168@gmail.com>
2025-09-10 22:00:46 +0800

    修改类

--- telegram/src/main/kotlin/com/github/kotlintelegrambot/network/Response.kt
+++ telegram/src/main/kotlin/com/github/kotlintelegrambot/network/Response.kt
@@ -4,7 +4,7 @@ import com.google.gson.annotations.SerializedName as Name
 
 data class Response<T>(
     val result: T?,
-    val ok: Boolean,
+    val ok: Boolean = false,
     @Name("error_code") val errorCode: Int? = null,
     @Name("description") val errorDescription: String? = null,
 )
